039660586614068a35de2830214ce32cdaa85bcd
AML <andrewmloop@gmail.com>
2022-02-22 09:23:56 -0600

    Add user schema

---
+++ server/models/User.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: [true, "Your email is required."],
+    lowercase: true,
+    match: [/\S+@\S+.\S+/, "Invalid Email."],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "A password is required."],
+  },
+  first_name: {
+    type: String,
+    required: [true, "Your first name is required."],
+  },
+  birth_date: {
+    type: Date,
+    required: [true, "Your birth date is required."]
+  },
+}, {timestamps: true});
+
+
+
+export default mongoose.model("User", UserSchema);
