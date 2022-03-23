8865768bc878d3859965ea718845995956063e38
AML <andrewmloop@gmail.com>
2022-03-23 11:47:11 -0400

    Refactor router
    Refactored App.js to route the login and register pages without any navigation components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Outlet } from "react-router-dom";
 import { Navbar } from "./components/navbar/Navbar";
 import { Banner } from "./components/banner/Banner";
 
@@ -12,64 +12,71 @@ import { Settings } from "./pages/Settings";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 
-function App() {
+export default function App() {
   const [exerciseList, setExerciseList] = useState([]);
   const [bannerText, setBannerText] = useState("");
 
   return (
-    <div className="relative h-screen bg-gray-600"> 
-      <Banner 
-        bannerText={bannerText} 
-      />
+    <div className="relative h-screen bg-gray-600">
       <Routes>
-        <Route path="/routine-list" element={
-          <RoutineList 
-            setBannerText={setBannerText}
-          />
-        }/>
-        <Route path="/exercise-list" element={
-          <ExerciseList 
-            setBannerText={setBannerText}
-            exerciseList={exerciseList}
-            setExerciseList={setExerciseList}
-          />
-        }/>
-        <Route path="/exercise" element={
-          <Exercise
-            setBannerText={setBannerText}
-          />
-        }/>
-        <Route path="/routine" element={
-          <Routine 
-            setBannerText={setBannerText}
-          />
-        }/>
-        <Route path="/log" element={
-          <Log 
-            setBannerText={setBannerText}
-          />
-        }/>
-        <Route path="/settings" element={
-          <Settings 
-            setBannerText={setBannerText}
-          />
-        }/>
-        <Route path="/login" element={
-          <Login 
-          />
-        }/>
-        <Route path="/" element={
-          <Login 
-          />
-        }/>
-        <Route path="/register" element={
-          <Register 
-          />
-        }/>
+        <Route element={<WithoutNav  />}>
+          <Route exact path="/" element={<Login />}/>
+          <Route exact path="/login" element={<Login />}/>
+          <Route exact path="/register" element={<Register />}/>
+        </Route>
+        <Route element={<WithNav bannerText={bannerText} />}>
+          <Route path="/routine-list" element={
+            <RoutineList 
+              setBannerText={setBannerText}
+            />
+          }/>
+          <Route path="/exercise-list" element={
+            <ExerciseList 
+              setBannerText={setBannerText}
+              exerciseList={exerciseList}
+              setExerciseList={setExerciseList}
+            />
+          }/>
+          <Route path="/exercise" element={
+            <Exercise
+              setBannerText={setBannerText}
+            />
+          }/>
+          <Route path="/routine" element={
+            <Routine 
+              setBannerText={setBannerText}
+            />
+          }/>
+          <Route path="/log" element={
+            <Log 
+              setBannerText={setBannerText}
+            />
+          }/>
+          <Route path="/settings" element={
+            <Settings 
+              setBannerText={setBannerText}
+            />
+          }/>
+        </Route>
       </Routes>
-      <Navbar />
     </div>
   );
 }
 
-export default App;
+function WithNav(props) {
+  return (
+    <>
+      <Banner bannerText={props.bannerText} />
+      <Outlet />
+      <Navbar />
+    </>
+  );
+}
+
+function WithoutNav() {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+}
