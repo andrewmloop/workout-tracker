b705c2fb24748f3a25bad910b9081da7dc63a865
aml <andrewmloop@gmail.com>
2024-02-15 19:56:03 -0500

    Remove all source files
    Removed all source files to create a clean slate that will allow for an easier move to newer versions of react and express

--- README.md
+++
@@ -1,9 +0,0 @@
-# Workout Tracker - [Link](https://iron-temple-app.herokuapp.com)
-
-A web app to build routines and record your progress at the gym.
-
-- ReactJS
-- JWT
-- ExpressJS
-- NodeJs
-- MongoDB
--- models/index.js
+++
@@ -1,9 +0,0 @@
-import User from "./User.js";
-import Exercise from "./Exercise.js";
-import Routine from "./Routine.js";
-import Log from "./Log.js";
-import Workout from "./Workout.js";
-
-const models = { User, Exercise, Routine, Log, Workout };
-
-export default models;
