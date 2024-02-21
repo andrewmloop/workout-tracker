5788d1cec7a856b451145ac0f5a4b96553dec2f4
aml <andrewmloop@gmail.com>
2024-02-20 22:45:12 -0500

    Add refactored register page

--- client/src/Client.jsx
+++ client/src/Client.jsx
@@ -1,24 +1,24 @@
 import React, { useState } from "react";
 import { Route, Routes, Outlet } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
-import ExerciseGroup from "./pages/ExerciseGroup";
-import ExerciseList from "./pages/ExerciseList";
-import ExerciseDetail from "./pages/ExerciseDetail";
-import RoutineList from "./pages/RoutineList";
-import RoutineDetail from "./pages/RoutineDetail";
-import Log from "./pages/Log";
-import Settings from "./pages/Settings";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import AddRoutine from "./pages/AddRoutine";
-import EditRoutine from "./pages/EditRoutine";
-import Chart from "./pages/Chart";
-import Notification from "./components/Notification";
+// import Navbar from "./components/Navbar";
+// import ExerciseGroup from "./pages/ExerciseGroup";
+// import ExerciseList from "./pages/ExerciseList";
+// import ExerciseDetail from "./pages/ExerciseDetail";
+// import RoutineList from "./pages/RoutineList";
+// import RoutineDetail from "./pages/RoutineDetail";
+// import Log from "./pages/Log";
+// import Settings from "./pages/Settings";
+// import Login from "./pages/Login";
+import Register from "./pages/register/Register";
+// import AddRoutine from "./pages/AddRoutine";
+// import EditRoutine from "./pages/EditRoutine";
+// import Chart from "./pages/Chart";
+// import Notification from "./components/Notification";
 
-import { UserProvider } from "./context/UserContext";
-import { NotifProvider } from "./context/NotificationContext";
-import { TimerContextProvider } from "./context/TimerContext";
+// import { UserProvider } from "./context/UserContext";
+import { NotifProvider } from "./store/NotificationStore";
+// import { TimerContextProvider } from "./context/TimerContext";
 
 export default function ClientUI() {
   // addMode allows the user to add exercises to a routine
@@ -30,61 +30,61 @@ export default function ClientUI() {
   const [newExercises, setNewExercises] = useState([]);
 
   return (
-    <UserProvider>
-      <NotifProvider>
-        <TimerContextProvider>
-          <div>
-            <Routes>
-              <Route element={<WithoutNav />}>
-                <Route exact path="/" element={<Login />} />
-                <Route exact path="/login" element={<Login />} />
-                <Route exact path="/register" element={<Register />} />
-              </Route>
-              <Route element={<WithNav />}>
-                <Route path="/exercise">
-                  <Route
-                    index
-                    path="/exercise"
-                    element={<ExerciseGroup addMode={addMode} />}
+    // <UserProvider>
+    <NotifProvider>
+      {/* <TimerContextProvider> */}
+      <div>
+        <Routes>
+          <Route element={<WithoutNav />}>
+            {/* <Route exact path="/" element={<Login />} />
+            <Route exact path="/login" element={<Login />} /> */}
+            <Route exact path="/register" element={<Register />} />
+          </Route>
+          {/* <Route element={<WithNav />}>
+            <Route path="/exercise">
+              <Route
+                index
+                path="/exercise"
+                element={<ExerciseGroup addMode={addMode} />}
+              />
+              <Route
+                path="list"
+                element={
+                  <ExerciseList
+                    addMode={addMode}
+                    setAddMode={setAddMode}
+                    activeRoutine={activeRoutine}
+                    newExercises={newExercises}
+                    setNewExercises={setNewExercises}
                   />
-                  <Route
-                    path="list"
-                    element={
-                      <ExerciseList
-                        addMode={addMode}
-                        setAddMode={setAddMode}
-                        activeRoutine={activeRoutine}
-                        newExercises={newExercises}
-                        setNewExercises={setNewExercises}
-                      />
-                    }
+                }
+              />
+              <Route path="detail" element={<ExerciseDetail />} />
+            </Route>
+            <Route path="/routine">
+              <Route index path="/routine" element={<RoutineList />} />
+              <Route path="add" element={<AddRoutine />} />
+              <Route path="update" element={<EditRoutine />} />
+              <Route
+                path="detail"
+                element={
+                  <RoutineDetail
+                    setAddMode={setAddMode}
+                    setActiveRoutine={setActiveRoutine}
                   />
-                  <Route path="detail" element={<ExerciseDetail />} />
-                </Route>
-                <Route path="/routine">
-                  <Route index path="/routine" element={<RoutineList />} />
-                  <Route path="add" element={<AddRoutine />} />
-                  <Route path="update" element={<EditRoutine />} />
-                  <Route
-                    path="detail"
-                    element={
-                      <RoutineDetail
-                        setAddMode={setAddMode}
-                        setActiveRoutine={setActiveRoutine}
-                      />
-                    }
-                  />
-                  <Route path="log" element={<Log />} />
-                </Route>
-                <Route exact path="/settings" element={<Settings />} />
-                <Route exact path="/chart" element={<Chart />} />
-              </Route>
-            </Routes>
-            <Notification />
-          </div>
-        </TimerContextProvider>
-      </NotifProvider>
-    </UserProvider>
+                }
+              />
+              <Route path="log" element={<Log />} />
+            </Route>
+            <Route exact path="/settings" element={<Settings />} />
+            <Route exact path="/chart" element={<Chart />} />
+          </Route> */}
+        </Routes>
+        {/* <Notification /> */}
+      </div>
+      {/* </TimerContextProvider> */}
+    </NotifProvider>
+    // </UserProvider>
   );
 }
 
---
+++ client/src/store/NotificationStore.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const NotifContext = React.createContext({});
+
+const NotifProvider = ({ children }) => {
+  const [notifStore, setNotifStore] = React.useState({});
+
+  return (
+    <NotifContext.Provider value={[notifStore, setNotifStore]}>
+      {children}
+    </NotifContext.Provider>
+  );
+};
+
+const useNotifStore = () => {
+  const [notifStore, setNotifStore] = React.useContext(NotifContext);
+
+  const handleNotif = (newNotifText, newNotifType, showNotif) => {
+    setNotifStore({
+      text: newNotifText,
+      // true == successful action
+      // false == failed action
+      type: newNotifType,
+      show: showNotif,
+    });
+  };
+
+  return { notifStore: notifStore, handleNotif: handleNotif };
+};
+
+export { NotifProvider, useNotifStore };
